62ab859c4a4163ea95cbc6429fc8e25e17973465
Marco Giammari <marcogiammari@gmail.com>
2023-04-27 20:37:08 +0200

    add(js) win condition

--- js/main.js
+++ js/main.js
@@ -47,26 +47,29 @@ function startGame(difficulty) {
 }
         
 function clickSquare() {
-            console.log(bombs);
-            hasBomb = false;
-            for (let j = 0; j < bombs.length; j++) {
-                const bomb = bombs[j];
-                if (bomb == (this.innerText)) {
-                    hasBomb = true;
-                } 
-            }
-            if (hasBomb) {
-                this.classList.add("exploded");
-                endGame();
-            } else {
-                this.classList.add("clicked");
-                points += 1;
-            }
-            this.removeEventListener("click", clickSquare);
+    let num = parseInt(document.querySelector("select").value);
+    hasBomb = false;
+    for (let j = 0; j < bombs.length; j++) {
+        const bomb = bombs[j];
+        if (bomb == (this.innerText)) {
+            hasBomb = true;
+        } 
+    }
+    if (hasBomb) {
+        this.classList.add("exploded");
+        endGame("defeat");
+    } else {
+        this.classList.add("clicked");
+        points += 1;
+        if (points == num - 16) {
+            endGame("win");
         }
+    }
+    this.removeEventListener("click", clickSquare);
+}
         
 
-function endGame() {
+function endGame(outcome) {
     squares = document.querySelectorAll(".square");
     for (let i = 0; i < squares.length; i++) {        
         squares[i].removeEventListener("click", clickSquare);
@@ -75,13 +78,13 @@ function endGame() {
     for (let i = 0; i < squares.length; i++) {
         for (let j = 0; j < bombs.length; j++) {
             if (squares[i].innerHTML == bombs[j]) {
-                console.log(squares[i], bombs[j])
                 squares[i].style.backgroundColor = "red"
             }
         }
     }
-    if (control) {
-        console.log("prova")
+    if (outcome == "defeat") {
+    result.innerText = `Game over. Punti totalizzati: ${points}`;
+    } else if (outcome == "win") {
+        result.innerText = `Congratulazioni, hai vinto! Punti totalizzati: ${points}`;
     }
-    result.innerText = (`Game over. Punti totalizzati: ${points}`);
 }
